f28bd2e95246dfa7d8cfb59e9eeda77ec4d2a023
OldiLo <ostapbataj79@gmail.com>
2026-06-12 19:53:08 +0300

    shitty linters

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -12,4 +12,7 @@ class Druid(Elf):
         return len(self._favourite_spell)
 
     def player_info(self) -> str:
-        return f"Druid {self.nickname}. {self.nickname} has a favourite spell: {self._favourite_spell}"
+        return (
+            f"Druid {self.nickname}. {self.nickname} has a "
+            f"favourite spell: {self._favourite_spell}"
+        )
