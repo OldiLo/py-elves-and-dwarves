070deb8f5e8a83a62d3810c6dec44906de13cfb7
OldiLo <ostapbataj79@gmail.com>
2026-06-12 19:46:04 +0300

    implemented player classes

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.elves.druid import Druid
+from app.players.elves.elf_ranger import ElfRanger
+
+
+def calculate_team_total_rating(
+    players: list[DwarfBlacksmith | DwarfWarrior | Druid | ElfRanger],
+) -> int:
+    return sum(player.get_rating() for player in players)
+
+
+def elves_concert(players: list[Druid | ElfRanger]) -> None:
+    [player.play_elf_song() for player in players]
+
+
+def feast_of_the_dwarves(
+    players: list[DwarfBlacksmith | DwarfWarrior],
+) -> None:
+    [player.eat_favourite_dish() for player in players]
